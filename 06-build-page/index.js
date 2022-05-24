0a3f6b61244e9bfce06f32ebf102602c29bf0ec5
Anatoliy Antonov <andrean-82@mail.ru>
2022-05-25 00:48:39 +0500

    feat: finished task 6. The assets folder is created and all the contents are copied. Build is assembled into the site.

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -5,13 +5,13 @@ const pathFile = path.resolve(__dirname, 'project-dist');
 const pathFile1 = path.resolve(pathFile, 'index.html');
 const pathComponents = path.resolve(__dirname, 'components');
 const pahtTemplate = path.resolve(__dirname, 'template.html');
-//const pathassetc = path.resolve(__dirname, 'assets');
-const pathcss = path.resolve(__dirname, 'styles'); // создание style.css
-const pathcsscopy = path.resolve(__dirname,'project-dist','style.css');   // создание style.css
-const writeFile = fs.createWriteStream(pathcsscopy, {encoding: 'utf-8'}); // создание style.css
+const pathassetc = path.resolve(__dirname, 'assets');
+const pathassetcNew = path.resolve(__dirname,'project-dist', 'assets');
+const pathcss = path.resolve(__dirname, 'styles'); // created style.css
+const pathcsscopy = path.resolve(__dirname,'project-dist','style.css');   // created style.css
+const writeFile = fs.createWriteStream(pathcsscopy, {encoding: 'utf-8'}); // created style.css
 
-fsPromises.mkdir(pathFile, {recursive: true}); // создание парки project-dist
-
+fsPromises.mkdir(pathFile, {recursive: true}); // creat folder project-dist
 
 const readHtml = fs.createReadStream(pahtTemplate, {encoding: 'utf-8'});
 
@@ -35,7 +35,7 @@ readHtml.on('end', () => {
           const primass = '{{'+ path.parse(element.name).name +'}}';
           filechunk = filechunk.replace(primass, elementfile);
           if (count === array.length) {
-            const fileHtml = fs.createWriteStream(pathFile1, {encoding: 'utf-8'}); // запуск стримма на запись дирикторию project-dist и создание index.html
+            const fileHtml = fs.createWriteStream(pathFile1, {encoding: 'utf-8'}); // start stream on the write directory project-dist and created index.html
             fileHtml.write(filechunk);
           }          
         });       
@@ -54,35 +54,30 @@ fs.readdir(pathcss, {encoding:'utf-8', withFileTypes: true}, (err, array) =>{
     }
   });
 });
- 
-
-// fs.readFile(path.join(pathComponents, element.name), {encoding: 'utf-8', withFileTypes: true}, (err, elementfile) => {
-//   if(err) throw err;
-//   const primass = '{{'+ path.parse(element.name).name +'}}';
-//   filechunk = filechunk.replace(primass, elementfile);
-//   console.log(filechunk.length);
-//   fileHtml.write(filechunk);
-// }); 
-
-// fs.stat(pathassetc, (err) => {
-//   if (!err) {
-//     fs.rm(pathassetc, {recursive: true}, (err) => {
-//       create();
-//     });
-//   } else {
-//     create();   
-//   }    
-// });
-// function create(){
-//   fs.readdir(pathFile1, {encoding:'utf-8', withFileTypes: true}, (err, array) => {
-//     if(err) throw err;
-//     fsPromises.mkdir(pathassetc, {recursive: true});
-//     array.forEach(element => {
-//       if(element.isFile()){
-//         fs.copyFile(path.join(pathFile1, element.name), path.join(pathassetc, element.name), (err) => {
-//           if(err) throw err;
-//         });
-//       }
-//     });
-//   });
-// }
+fs.stat(pathassetcNew, (err) => { // сheck the existence of the folder assets
+  if(!err) {  
+    fs.rm(pathassetcNew, {recursive: true}, () => {
+      mkd();
+    });
+  }else{
+    mkd();
+  }  
+});
+function mkd(){
+  fsPromises.mkdir(pathassetcNew, {recursive: true}); //created folder assets in project-dist
+}
+function create(folderpath, folderpathNew){   
+  fs.readdir(folderpath, {encoding:'utf-8', withFileTypes: true}, (err, array) => {
+    for (let elem of array){
+      if(elem.isDirectory()){
+        fsPromises.mkdir(path.join(folderpathNew, elem.name), {recursive: true});
+        create(path.join(folderpath, elem.name), path.join(folderpathNew, elem.name));  // recursive function
+      }else{
+        fs.copyFile(path.join(folderpath, elem.name), path.join(folderpathNew, elem.name), (err) => {
+          if(err) throw err;
+        });
+      }        
+    }
+  });
+}
+create(pathassetc, pathassetcNew);
